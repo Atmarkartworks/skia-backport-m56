88fbb1d6d21a08719e23b9851b8fe844045f8745
tmiura <tmiura@atmarkartworks.com>
2023-03-01 12:19:09 +0900

    move unicode files

---
+++ chgcode/chguni.bat
@@ -0,0 +1,10 @@
+@echo off
+copy gm\unicode\verttext.cpp ..\gm\verttext.cpp
+copy bench\unicode\nanobench.cpp ..\bench\nanobench.cpp
+copy tools\unicode\Stats.h ..\tools\Stats.h
+copy tools\unicode\sk_tool_utils.cpp ..\tools\sk_tool_utils.cpp
+copy src\effects\unicode\SkDisplacementMapEffect.cpp ..\src\effects\SkDisplacementMapEffect.cpp
+copy tests\unicode\FontHostTest.cpp ..\tests\FontHostTest.cpp
+
+
+
---
+++ chgcode/chgutf8.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cp gm/utf-8/verttext.cpp ../gm/verttext.cpp
+cp bench/utf-8/nanobench.cpp ../bench/nanobench.cpp
+cp tools/utf-8/Stats.h ../tools/Stats.h
+cp tools/utf-8/sk_tool_utils.cpp ../tools/sk_tool_utils.cpp
+cp src/effects/utf-8/SkDisplacementMapEffect.cpp ../src/effects/SkDisplacementMapEffect.cpp
+cp tests/utf-8/FontHostTest.cpp ../tests/FontHostTest.cpp
+
+
+
+
